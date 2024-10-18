1fc4b438480c2d1317bfb9c0e7bda6b4e48e8296
Dharmadeep <dharmadeepmadisetty@gmail.com>
2024-10-18 21:44:21 +0530

    Deployment code mermaid

--- client/src/actions/project.js
+++ client/src/actions/project.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({ baseURL: "https://dev-labs-server.vercel.app" });
 
 export const submitProject = (projectData, navigate) => async (dispatch) => {
   try {
